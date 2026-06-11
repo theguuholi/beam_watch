b25caab070fd28da7a1f1cba53fcc339b0a54427
Gustavo Oliveira <g.92oliveira@gmail.com>
2026-06-11 14:26:48 -0300

    refactor: remove redundant length guards in LogParser.extract_fields/1

--- lib/beamwatch/log_parser.ex
+++ lib/beamwatch/log_parser.ex
@@ -43,11 +43,8 @@ defmodule BeamWatch.LogParser do
     |> Regex.scan(payload)
     |> Enum.reduce(%{}, fn match, acc ->
       case match do
-        # Quoted value (3 elements): [full_match, key, quoted_value]
-        [_, key, quoted] when length(match) == 3 -> Map.put(acc, key, quoted)
-        # Unquoted value (4 elements): [full_match, key, "", unquoted_value]
-        [_, key, "", value] when length(match) == 4 -> Map.put(acc, key, value)
-        # Catch-all for other patterns
+        [_, key, quoted] -> Map.put(acc, key, quoted)
+        [_, key, "", value] -> Map.put(acc, key, value)
         _ -> acc
       end
     end)
